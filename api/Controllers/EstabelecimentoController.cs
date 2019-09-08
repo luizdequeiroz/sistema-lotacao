68c3f8b4afdfb7020e482dc7c88ad1bf003d679c
LuizDeQueiroz <luizdequeiroz@outlook.com.br>
2019-09-08 01:42:35 -0300

    commit inicial, subindo projeto criado junto com Jon do GoHorse.

---
+++ api/Controllers/EstabelecimentoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstabelecimentoController : ControllerBase
+    {
+        [HttpGet("{quantidade}/{latitude}/{longitude}")]
+        public object List(int quantidade, string latitude, string longitude)
+        {
+            return "Hello, " + nome + " World!";
+        }
+    }
+}
